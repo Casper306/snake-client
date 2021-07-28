c024cfcb4c1ef4157c7c3f30c5d7a59fb5db13c0
casper306 <perfect306@gmail.com>
2021-07-28 20:31:19 +0000

    add client.js and modified play.js

---
+++ client.js
@@ -0,0 +1,17 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '135.23.223.133',// IP address here,
+    port: 50542// PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+conn.on('data', () =>{
+  console.log("you ded cuz you idled");
+})
+  return conn;
+};
+
+module.exports = {connect};
--- play.js
+++ play.js
@@ -1,19 +1,9 @@
-const net = require("net");
+const {connect} = require("./client.js");
+
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '135.23.223.133',// IP address here,
-    port: 50542// PORT number here,
-  });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-conn.on('data', () =>{
-  console.log("you ded cuz you idled");
-})
-  return conn;
-};
+
 
 console.log("Connecting ...");
 connect();
