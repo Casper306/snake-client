cf8f7f3e5434749e8f578fb9b5eb2b00fac45454
casper306 <perfect306@gmail.com>
2021-07-28 22:29:39 +0000

    modified client.js with stdin

--- play.js
+++ play.js
@@ -1,9 +1,24 @@
 const {connect} = require("./client.js");
 
 
-// establishes a connection with the game server
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+  
+};
+
+const stdin = setupInput();
 
 
+const handleUserInput = function (key) { 
+    if(key === '\u0003') {
+      process.exit();
+    }
+};
 
+stdin.on("data", handleUserInput);
 console.log("Connecting ...");
 connect();
