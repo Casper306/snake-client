bbef7c00096e2ac0a55a720add673b8696185145
casper306 <perfect306@gmail.com>
2021-07-29 01:42:04 +0000

    modified play.js and add input.js with ctrl c key

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const {connect} = require("./client.js");
+const { setupInput } = require("./input");
 
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  return stdin;
-  
-};
-
-const stdin = setupInput();
-
-
-const handleUserInput = function (key) { 
-    if(key === '\u0003') {
-      process.exit();
-    }
-};
-
-stdin.on("data", handleUserInput);
 console.log("Connecting ...");
 connect();
+setupInput();
